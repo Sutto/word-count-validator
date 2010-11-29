162a265cc096b853ea3b989dec69831ed2e0e72e
Eric Anderson <eric@pixelwareinc.com>
2010-11-30 06:08:39 +0800

    Move most normalization to initialize as hash is frozen by argument check.
    Left purely argument checking in check_validity! I actually like the way
    the plugin originally did it and don't think Rails should freeze until
    after validation. Thought about simply duping the option has and assigning
    to the instance variable then refreezing but that seems un-official and
    probably a better solution is to fix Rails.

--- lib/active_model/validations/word_count.rb
+++ lib/active_model/validations/word_count.rb
@@ -7,28 +7,34 @@ module ActiveModel
     
     class WordCountValidator < ActiveModel::EachValidator
 
-      def check_validity!
-        if options.has_key?(:in)
-          range = options.delete(:in)
-          unless range.present? && range.respond_to?(:min) && range.respond_to?(:max)
-            raise ArgumentError, "You must provide a valid range for the number of words e.g. 2..100"
-          end
-          options[:min], options[:max] = range.min, range.max
-        end
+      # Provide option defaults and normalize options. This is done
+      # prior to check_validity! as options becomes frozen and
+      # check_validity! can only report errors but not modify the
+      # options.
+      def initialize(options)
         options[:min] = options.delete(:minimum) if options.has_key?(:minimum)
         options[:max] = options.delete(:maximum) if options.has_key?(:maximum)
         options[:max] ||= 100
         options[:min] ||= 0
+        options[:strip_tags] = true
+        if options.has_key?(:in)
+          range = options[:in]
+          if range.present? && range.respond_to?(:min) && range.respond_to?(:max)
+            options[:min], options[:max] = range.min, range.max
+            options.delete :in
+          end
+        end
+        super
+      end
+
+      def check_validity!
+        raise ArgumentError, "You must provide a valid range for the number of words e.g. 2..100" if options.has_key?(:in)
         raise ArgumentError, "The min value must respond to to_i" unless options[:min].respond_to?(:to_i)
         raise ArgumentError, "The max value must respond to to_i" unless options[:max].respond_to?(:to_i)
-        # Finally, normalize the min values.
-        options[:min] = options[:min].to_i
-        options[:max] = options[:max].to_i
-        options[:strip_tags] = true
       end
 
       def validate_each(record, attribute, value)
-        min_words, max_words  = options[:min], options[:max]
+        min_words, max_words  = options[:min].to_i, options[:max].to_i
         value = ActionController::Base.helpers.strip_tags value if options[:strip_tags]
         count = word_count_for(value)
         if !options[:skip_min] && count < min_words
